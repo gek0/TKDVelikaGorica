7d423d5c80ba06696c22f18403a84b4aa09ceb94
Matija Buriša <matijaburisa@gmail.com>
2018-03-06 18:39:35 +0100

    about us section done
    public homepage finished

--- app/views/public/home/about-us.blade.php
+++ app/views/public/home/about-us.blade.php
@@ -1,10 +1,14 @@
 <section id="about-us">
     @if($about_us_data->count() > 0)
-        <h2 class="text-center inverted-title incremented-title">Najbitnije što morate znati o nama</h2>
+        <h2 class="text-center inverted-title incremented-title">{{ $about_us_data->about_title }}</h2>
 
-        <blockquote>
-            {{ removeEmptyP(nl2p((new BBCParser)->parse($about_us_data['about_body']))) }}
-        </blockquote>
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+                <blockquote class="about" cite="{{ getenv('WEB_NAME') }}">
+                    {{ removeEmptyP(nl2p((new BBCParser)->parse($about_us_data->about_body))) }}
+                </blockquote>
+            </div>
+        </div>
     @else
         <h2 class="text-center inverted-title">Trenutno nemamo više informacija o nama, navratite uskoro.</h2>
     @endif
